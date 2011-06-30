1da28c80d2da3a66933aac22467692fbbd02b394
mikael.barbero@gmail.com <mikael.barbero@gmail.com@bed2a7c0-1b16-4b63-f10c-2a79fe099e5c>
2011-06-30 17:12:12 +0000

    fixed build properties fixed javadoc

--- trunk/org.eclipselabs.emfpath/src/org/eclipselabs/emfpath/base/Equivalences2.java
+++ trunk/org.eclipselabs.emfpath/src/org/eclipselabs/emfpath/base/Equivalences2.java
@@ -12,11 +12,15 @@ package org.eclipselabs.emfpath.base;
 
 import com.google.common.base.Equivalence;
 import com.google.common.base.Equivalences;
+import com.google.common.base.Preconditions;
 import com.google.common.base.Predicate;
 
+import javax.annotation.Nullable;
+
 /**
- * @author mbarbero
- *
+ * Set of utility methods to create {@link Predicate} from an {@link Equivalence} strategy.
+ * 
+ * @author <a href="mailto:mikael.barbero@obeo.fr">Mikaël Barbero</a>
  */
 public final class Equivalences2 {
 
@@ -25,11 +29,16 @@ public final class Equivalences2 {
 	}
 
 	/**
-	 * @param equivalenceStrategy
-	 * @param to
-	 * @return
+	 * Creates a {@link Predicate} that test the equivalence to <code>to</code> following the given
+	 * <code>equivalenceStrategy</code>.
+	 * 
+	 * @param equivalenceStrategy the strategy to use when applying the predicate.
+	 * @param to the object to test the equivalence against.
+	 * @return a new {@link Predicate}.
+	 * @throws NullPointerException if the given <code>equivalenceStrategy</code> is null.
 	 */
-	public static <T> Predicate<T> equivalencePredicate(final Equivalence<? super T> equivalenceStrategy, final T to) {
+	public static <T> Predicate<T> equivalencePredicate(final Equivalence<? super T> equivalenceStrategy, @Nullable final T to) {
+		Preconditions.checkNotNull(equivalenceStrategy);
 		return new Predicate<T>() {
 			public boolean apply(T input) {
 				return equivalenceStrategy.equivalent(to, input);
@@ -38,10 +47,13 @@ public final class Equivalences2 {
 	}
 
 	/**
-	 * @param to
-	 * @return
+	 * Creates a {@link Predicate} that test the equality to <code>to</code> following the {@link Equivalences#equals()}
+	 * strategy.
+	 * 
+	 * @param to the object to test the equality against.
+	 * @return a new {@link Predicate}.
 	 */
-	public static <T> Predicate<T> equalsTo(final T to) {
+	public static <T> Predicate<T> equalsTo(@Nullable final T to) {
 		return new Predicate<T>() {
 			public boolean apply(T input) {
 				return Equivalences.equals().equivalent(to, input);
@@ -50,10 +62,13 @@ public final class Equivalences2 {
 	}
 
 	/**
-	 * @param to
-	 * @return
+	 * Creates a {@link Predicate} that test the equality to <code>to</code> following the
+	 * {@link Equivalences#identity()} strategy.
+	 * 
+	 * @param to the object to test the equality against.
+	 * @return a new {@link Predicate}.
 	 */
-	public static <T> Predicate<T> sameIdentityAs(final T to) {
+	public static <T> Predicate<T> sameIdentityAs(@Nullable final T to) {
 		return new Predicate<T>() {
 			public boolean apply(T input) {
 				return Equivalences.identity().equivalent(to, input);
